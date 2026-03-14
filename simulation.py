9610b2fac5fe3ae9a9c3fcfd650cfe71fc33c9e6
wudi <wudi@dexforce.com>
2026-03-14 23:45:01 +0800

    添加众擎原始的assets，以便对齐

--- simulation.py
+++ simulation.py
@@ -2,7 +2,8 @@ import mujoco
 import mujoco.viewer
 from pathlib import Path
 
-SCENE_PATH = Path(__file__).parent / "robot/pm01.xml"
+#SCENE_PATH = Path(__file__).parent / "robot/pm01.xml"
+SCENE_PATH = Path(__file__).parent / "assets/resource/pm_v2.xml"
 
 model = mujoco.MjModel.from_xml_path(str(SCENE_PATH))
 data = mujoco.MjData(model)
